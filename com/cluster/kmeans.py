a1ae2e3a7418af18074abdeb23453c7cc704291c
zhangzhen <zhangzhen@mininglamp.com>
2017-04-25 16:00:45 +0800

    init

---
+++ com/cluster/kmeans.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/4/25 下午3:59
+# @Author  : zhangzhen
+# @Site    : 
+# @File    : kmeans.py
+# @Software: PyCharm
+
+if __name__ == '__main__':
+    print ''
